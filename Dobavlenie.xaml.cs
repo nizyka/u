790477689c565aef0cf2fc4724f78f57deac38bd
nizyka <73965065+nizyka@users.noreply.github.com>
2022-02-28 16:06:39 +0300

    Add files via upload

---
+++ Dobavlenie.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StorIS
+{
+    /// <summary>
+    /// Логика взаимодействия для Dobavlenie.xaml
+    /// </summary>
+    public partial class Dobavlenie : Window
+    {
+        
+        public Dobavlenie()
+        {
+            InitializeComponent();
+            ComboBox1.ItemsSource = LearnDBEntites.GetContext().Postavka.ToList();         
+            ComboBox2.ItemsSource = LearnDBEntites.GetContext().Kategorii_tovarov.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (tovarComboBox.SelectedText == null)
+                errors.AppendLine("Укажите название товара!");
+            if (dataP.SelectedDate == null)
+                errors.AppendLine("Укажите окончание срока годности товара!");
+            if (textBox.SelectedText == null)
+                errors.AppendLine("Укажите количество товара!");
+            if (textBox1.SelectedText == null)
+                errors.AppendLine("Укажите цену товара!");
+            if (ComboBox1.SelectedItem == null)
+                errors.AppendLine("Укажите ID поставки товара!");
+            if (ComboBox2.SelectedItem == null)
+                errors.AppendLine("Укажите ID категории товара!");
+            if(errors.Length >0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            LearnDBEntites.GetContext().Tovari.Add(new Tovari()
+            {
+                Наименование_товара = tovarComboBox.SelectedText,
+                Дата_окончания_сг = dataP.SelectedDate.Value,
+                Количество = Convert.ToInt32(textBox.SelectedText),
+                Цена = Convert.ToInt32(textBox1.SelectedText),
+                ID_поставки = Convert.ToInt32(ComboBox1.SelectedItem),
+                ID_категории = Convert.ToInt32(ComboBox2.SelectedItem),
+            });
+            LearnDBEntites.GetContext().SaveChanges();
+            ((Avtorizacia)this.Owner).UpdateData();
+            MessageBox.Show("Данные успешно добавлены!");
+            this.Close();
+
+
+            
+
+        }
+    }
+}
---
+++ Redactirovanie.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StorIS
+{
+    /// <summary>
+    /// Логика взаимодействия для Redactirovanie.xaml
+    /// </summary>
+    public partial class Redactirovanie : Window
+    {
+        public Redactirovanie(Tovari tovari)
+        {
+            InitializeComponent();
+
+            DataContext = tovari;
+            tovarComboBox.ItemsSource = LearnDBEntites.GetContext().Tovari.ToList();
+
+            tovarComboBox.Text = tovari.Наименование_товара;
+            textBox.Text = tovari.Количество.ToString();
+            textBox1.Text = tovari.Цена.ToString();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LearnDBEntites.GetContext().SaveChanges();
+            ((Avtorizacia)this.Owner).UpdateData();
+            this.Close();
+            MessageBox.Show("");
+        }
+    }
+}
